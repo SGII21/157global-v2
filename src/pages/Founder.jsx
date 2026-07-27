aaa5b3ddee80d2d2ebae24a3838973150230b024
SGII21 <sg@157Global.com>
2026-07-27 16:16:55 -0400

    Update Founder.jsx

--- src/pages/Founder.jsx
+++ src/pages/Founder.jsx
@@ -43,7 +43,7 @@ export default function Founder() {
         </p>
 
         <img
-          src="/Headshot Jul 27, 2026, 03_58_35 PM.png"
+          src="/HeadshotJul272026.png"
           alt="Skip Glass"
           className="mx-auto my-10 block h-auto max-h-[450px] w-auto"
         />
